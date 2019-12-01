02e67a2a4d7277a7961750affd4c247d207fb69c
Kevin Lewis <lewisks3@vcu.edu>
2019-12-01 15:42:03 -0500

    Baseline
    Basic files created after creating project.

---
+++ cmake-build-debug/CMakeFiles/INFO450Encrypt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/INFO450Encrypt.dir/main.cpp.o"
+  "INFO450Encrypt"
+  "INFO450Encrypt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/INFO450Encrypt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
